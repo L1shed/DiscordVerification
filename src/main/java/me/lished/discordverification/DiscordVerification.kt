db77d02cfa4e15dc4e2eef60ed7111dcc7ea0e15
BuildTools <skywarspanel@gmail.com>
2024-02-08 21:36:00 +0100

    going bed with my working bot

--- src/main/java/me/lished/discordverification/DiscordVerification.kt
+++ src/main/java/me/lished/discordverification/DiscordVerification.kt
@@ -1,5 +1,7 @@
 package me.lished.discordverification
 
+import me.jakejmattson.discordkt.commands.commands
+import me.jakejmattson.discordkt.dsl.bot
 import me.lished.discordverification.commands.VerifyCommand
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -7,5 +9,18 @@ class DiscordVerification : JavaPlugin() {
     override fun onEnable() {
         getCommand("verify")?.setExecutor(VerifyCommand())
         saveConfig() // or saveDefaultConfig() i'm brainfucking brainfucked
+
+        val token = ""
+        bot(token) {
+            prefix { "+" }
+        }
+    }
+
+    fun demo() = commands("testouille") {
+        slash("Verify", "A VVV command.") {
+            execute {
+                respond("Verify!")
+            }
+        }
     }
 }
