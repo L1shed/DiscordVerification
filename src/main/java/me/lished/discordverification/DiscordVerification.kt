37939e8b3aeaa98ffebe5ab46f11598d7644b0b7
BuildTools <skywarspanel@gmail.com>
2024-02-08 20:19:05 +0100

    Initial commit

---
+++ src/main/java/me/lished/discordverification/DiscordVerification.kt
@@ -0,0 +1,13 @@
+package me.lished.discordverification
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DiscordVerification : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/lished/discordverification/managers/VerificationManager.kt
@@ -0,0 +1,4 @@
+package me.lished.discordverification.managers
+
+class VerifyManager {
+}
