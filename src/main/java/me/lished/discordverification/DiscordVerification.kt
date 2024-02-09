952ff128a0fb3a70a35064aa6dbb98c6778d7800
BuildTools <skywarspanel@gmail.com>
2024-02-09 08:16:41 +0100

    trying changes

--- src/main/java/me/lished/discordverification/DiscordVerification.kt
+++ src/main/java/me/lished/discordverification/DiscordVerification.kt
@@ -1,8 +1,10 @@
 package me.lished.discordverification
 
+import me.jakejmattson.discordkt.arguments.IntegerArg
 import me.jakejmattson.discordkt.commands.commands
 import me.jakejmattson.discordkt.dsl.bot
 import me.lished.discordverification.commands.VerifyCommand
+import me.lished.discordverification.managers.VerificationManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class DiscordVerification : JavaPlugin() {
@@ -10,16 +12,17 @@ class DiscordVerification : JavaPlugin() {
         getCommand("verify")?.setExecutor(VerifyCommand())
         saveConfig() // or saveDefaultConfig() i'm brainfucking brainfucked
 
-        val token = ""
-        bot(token) {
+        bot(config.getString("token")) {
             prefix { "+" }
         }
     }
 
     fun demo() = commands("testouille") {
-        slash("Verify", "A VVV command.") {
-            execute {
-                respond("Verify!")
+        slash("Verify", "A Verify command.") {
+            execute(IntegerArg("Code")) {
+                val code = args.toString()
+
+                respond("Verified")
             }
         }
     }
--- src/main/java/me/lished/discordverification/managers/VerificationManager.kt
+++ src/main/java/me/lished/discordverification/managers/VerificationManager.kt
@@ -3,11 +3,12 @@ import org.bukkit.entity.Player
 import java.util.*
 import kotlin.collections.HashMap
 
-data class VerificationData(val code: String, val expiration: Date)
+//data class VerificationData(val code: String, val expiration: Date)
 
 object VerificationManager {
 
-    val verificationCodes: MutableMap<Player, VerificationData> = HashMap()
+    val verificationCodes: MutableMap<Player, String> = HashMap()
+    val expiration: MutableMap<String, Date> = HashMap()
 
     fun generateVerificationCode(): String {
         return (100000..999999).random().toString()
