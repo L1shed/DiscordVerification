3d3899001d57238bdad61c7fd85adbc4ccffdcf2
BuildTools <skywarspanel@gmail.com>
2024-02-11 18:45:25 +0100

    Move to JDA

--- src/main/java/me/lished/discordverification/DiscordVerification.kt
+++ src/main/java/me/lished/discordverification/DiscordVerification.kt
@@ -2,6 +2,7 @@ package me.lished.discordverification
 
 import me.lished.discordverification.commands.VerifyCommand
 import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.hooks.ListenerAdapter
 import org.bukkit.plugin.java.JavaPlugin
 
 class DiscordVerification : JavaPlugin() {
@@ -9,7 +10,28 @@ class DiscordVerification : JavaPlugin() {
         getCommand("verify")?.setExecutor(VerifyCommand())
         saveConfig()
 
-        val jda = JDABuilder.createDefault(config.getString("token")).build()
+        val jda = JDABuilder.createDefault(config.getString("token"))
+            .addEventListeners(VerifyCommannd())
+            .build()
+    }
+}
+
+class VerifyCommannd : ListenerAdapter() {
+    override fun onSlashCommand(event: SlashCommandEvent) {
+        if (event.name == "verify") {
+            val arg = event.getOption("number")?.asString
+            if (arg != null) {
+                try {
+                    val number = arg.toInt()
+                    // Perform verification with the provided number
+                    event.reply("Verification successful for number: $number").queue()
+                } catch (e: NumberFormatException) {
+                    event.reply("Invalid number format.").queue()
+                }
+            } else {
+                event.reply("Please provide a number.").queue()
+            }
+        }
     }
 }
 
