439ae6a3303f3a14be7f717e051c102a6efad624
BuildTools <skywarspanel@gmail.com>
2024-02-11 20:21:38 +0100

    Ignored Vuln + Added DISCORD4J

--- src/main/java/me/lished/discordverification/DiscordVerification.kt
+++ src/main/java/me/lished/discordverification/DiscordVerification.kt
@@ -1,24 +1,49 @@
 package me.lished.discordverification
 
+import discord4j.core.DiscordClient
+import discord4j.core.GatewayDiscordClient
+import discord4j.core.event.ReactiveEventAdapter
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
+import discord4j.core.event.domain.lifecycle.ReadyEvent
+import discord4j.core.`object`.command.ApplicationCommandOption
+import discord4j.discordjson.json.ApplicationCommandOptionData
+import discord4j.discordjson.json.ApplicationCommandRequest
 import me.lished.discordverification.commands.VerifyCommand
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.interactions.commands.OptionType
-import net.dv8tion.jda.api.interactions.commands.build.Commands
 import org.bukkit.plugin.java.JavaPlugin
+import org.reactivestreams.Publisher
+import reactor.core.publisher.Mono
+
 
 class DiscordVerification : JavaPlugin() {
     override fun onEnable() {
         getCommand("verify")?.setExecutor(VerifyCommand())
         saveConfig()
 
-        val jda = JDABuilder.createDefault(config.getString("token"))
-            .build()
+        val client: GatewayDiscordClient? = DiscordClient.create(config.getString("token")!!)
+            .login()
+            .block()
 
-        jda.updateCommands().addCommands(
-            Commands.slash("verify", "Verify to be secure")
-                .setGuildOnly(true)
-                .addOption(OptionType.INTEGER, "code", "The code you got in Minecraft", true)
-        ).queue();
+        val verifyCmdRequest: ApplicationCommandRequest = ApplicationCommandRequest.builder()
+            .name("verify")
+            .description("Verify your Minecraft account")
+            .addOption(
+                ApplicationCommandOptionData.builder()
+                    .name("code")
+                    .description("The code you got in the chat")
+                    .type(ApplicationCommandOption.Type.INTEGER.value)
+                    .required(true)
+                    .build()
+            ).build()
+
+        client?.on(object : ReactiveEventAdapter() {
+
+            override fun onChatInputInteraction(event: ChatInputInteractionEvent): Publisher<*> {
+                if (event.commandName == "random") {
+                    return event.reply("caca")
+                }
+                return Mono.empty<Any>()
+            }
+        })?.blockLast()
     }
 }
 
