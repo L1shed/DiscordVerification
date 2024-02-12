b4fbf8af5dbc8b1218021e5febeaa15e851a9d84
BuildTools <skywarspanel@gmail.com>
2024-02-12 14:07:04 +0100

    Clean files

--- src/main/java/me/lished/discordverification/DiscordVerification.kt
+++ src/main/java/me/lished/discordverification/DiscordVerification.kt
@@ -1,15 +1,8 @@
 package me.lished.discordverification
 
-import discord4j.core.DiscordClient
-import discord4j.core.GatewayDiscordClient
-import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
-import discord4j.core.`object`.command.ApplicationCommandOption
-import discord4j.discordjson.json.ApplicationCommandOptionData
-import discord4j.discordjson.json.ApplicationCommandRequest
 import me.lished.discordverification.commands.VerifyCommand
+import me.lished.discordverification.managers.DiscordBot
 import org.bukkit.plugin.java.JavaPlugin
-import org.reactivestreams.Publisher
-import java.util.function.Function
 
 
 class DiscordVerification : JavaPlugin() {
@@ -17,29 +10,7 @@ class DiscordVerification : JavaPlugin() {
         getCommand("verify")?.setExecutor(VerifyCommand())
         saveConfig()
 
-        val client: GatewayDiscordClient? = DiscordClient.create(config.getString("token")!!)
-            .login()
-            .block()
-
-        val verifyCmdRequest: ApplicationCommandRequest = ApplicationCommandRequest.builder()
-            .name("verify")
-            .description("Verify your Minecraft account")
-            .addOption(
-                ApplicationCommandOptionData.builder()
-                    .name("code")
-                    .description("The code you got in the chat")
-                    .type(ApplicationCommandOption.Type.INTEGER.value)
-                    .required(true)
-                    .build()
-            ).build()
-
-        client!!.on(ChatInputInteractionEvent::class.java)
-            .subscribe { event ->
-                if (event.commandName == "verify") {
-                    event.reply("d")
-                        .subscribe()
-                }
-            }
+        DiscordBot.startBot(config.getString("token")!!)
 
     }
 }
---
+++ src/main/java/me/lished/discordverification/managers/DiscordBot.kt
@@ -0,0 +1,37 @@
+package me.lished.discordverification.managers
+
+import discord4j.core.DiscordClient
+import discord4j.core.GatewayDiscordClient
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
+import discord4j.core.`object`.command.ApplicationCommandOption
+import discord4j.discordjson.json.ApplicationCommandOptionData
+import discord4j.discordjson.json.ApplicationCommandRequest
+
+object DiscordBot {
+    fun startBot(token: String) {
+        val client: GatewayDiscordClient? = DiscordClient.create(token)
+            .login()
+            .block()
+
+        val verifyCmdRequest: ApplicationCommandRequest = ApplicationCommandRequest.builder()
+            .name("verify")
+            .description("Verify your Minecraft account")
+            .addOption(
+                ApplicationCommandOptionData.builder()
+                    .name("code")
+                    .description("The code you got in the chat")
+                    .type(ApplicationCommandOption.Type.INTEGER.value)
+                    .required(true)
+                    .build()
+            ).build()
+
+        client!!.on(ChatInputInteractionEvent::class.java)
+            .subscribe { event ->
+                if (event.commandName == "verify") {
+                    event.reply("d")
+                        .withEphemeral(true)
+                        .subscribe()
+                }
+            }
+    }
+}
