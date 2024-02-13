1adb16a20868210b6d83a863262bb6a7146225d1
BuildTools <skywarspanel@gmail.com>
2024-02-13 19:19:42 +0100

    Discord Bot

--- src/main/java/me/lished/discordverification/managers/DiscordBot.kt
+++ src/main/java/me/lished/discordverification/managers/DiscordBot.kt
@@ -33,7 +33,7 @@ object DiscordBot {
                         .map { it.asLong().toInt() }
                         .orElse(1))
                     if (player != null) event.reply("verified").withEphemeral(true).subscribe()
-                    event.reply("goblet").withEphemeral(true).subscribe()
+                    event.reply("growl").withEphemeral(true).subscribe()
                 }
             }
     }
