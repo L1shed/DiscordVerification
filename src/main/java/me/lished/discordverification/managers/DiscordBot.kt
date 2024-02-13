4927255b21d2403b7933c52429a8d4dcf6129d7b
BuildTools <skywarspanel@gmail.com>
2024-02-13 19:20:36 +0100

    Discord Bot

--- src/main/java/me/lished/discordverification/managers/DiscordBot.kt
+++ src/main/java/me/lished/discordverification/managers/DiscordBot.kt
@@ -33,7 +33,7 @@ object DiscordBot {
                         .map { it.asLong().toInt() }
                         .orElse(1))
                     if (player != null) event.reply("verified").withEphemeral(true).subscribe()
-                    event.reply("let's go!").withEphemeral(true).subscribe()
+                    event.reply("pulled").withEphemeral(true).subscribe()
                 }
             }
     }
